384b4560f916c63a0e53f766ee476b2fdf3ceb87
Vsevlad Selin <vsevladselin@gmail.com>
2026-05-26 19:23:17 +0300

    Add: rectangle area function

--- geometry.py
+++ geometry.py
@@ -3,5 +3,9 @@ import math
 def calculate_area(radius):
     return math.pi * radius ** 2
 
+def calculate_rectangle_area(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print(f"Area: {calculate_area(5)}")
+
